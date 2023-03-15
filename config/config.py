4675b3e1dd36bbe28f798da91e9ea5d6514fc0a6
teamlavcoder <127596317+teamlavcoder@users.noreply.github.com>
2023-03-15 20:03:02 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -30,7 +30,7 @@ UPSTREAM_REPO = getenv(
     "https://github.com/its-star-boi/ShizukaXMusic ",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GITHUB_REPO = getenv("GITHUB_REPO", "https://t.me/feelings_of_lav")
+GITHUB_REPO = getenv("GITHUB_REPO", " https://github.com/its-star-boi/ShizukaXMusic")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Star_X_Network")
